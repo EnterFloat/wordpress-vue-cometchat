00ffda4baec91b7998a5ffb9e339a069050fea80
Kasper Skov Hansen <kasperskov01@gmail.com>
2020-09-18 22:10:08 +0200

    Vue CometChat integrated in WP plugin.

---
+++ src/CONSTS.js
@@ -0,0 +1,6 @@
+export const COMETCHAT_CONSTANTS = {
+    APP_ID: '23200bdf29727b1',
+    REGION: 'eu',
+    API_KEY: '281d58e6467af77d3b7cd20fad36ee2153aec631'
+}
+
---
+++ src/lib/cometchat-components/components/Log.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>{{log(log_data)}}</div>
+</template>
+<script>
+export default {
+    name: "Log",
+    props:['log_data'],
+    data() {
+        return {
+            log: (log_data) => {
+                console.log(log_data)
+            }
+        }
+    },
+
+}
+</script>
